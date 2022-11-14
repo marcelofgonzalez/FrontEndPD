13e42334c706f7fa00d043f352e0500e114a5a16
marcelofgonzalez <113860795+marcelofgonzalez@users.noreply.github.com>
2022-11-13 21:52:37 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function mfg() {
+  alert("Marcelo Gonzalez");
+}
+function oscuro() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.backgroundImage = "none";
+  document.body.style.color = "white";
+  document.getElementById(1).style.color = "white";
+  document.getElementById(2).style.color = "white";
+  document.getElementById(3).style.color = "white";
+  document.getElementById(4).style.color = "white";
+  document.getElementById(5).style.color = "white";
+  document.getElementById(6).style.color = "white";
+  document.getElementById(7).style.color = "white";
+  document.getElementById(8).style.color = "white";
+  document.getElementById(9).style.color = "white";
+  document.getElementById(10).style.color = "white";
+  document.getElementById(11).style.color = "white";
+  document.getElementById(12).style.color = "white";
+}
